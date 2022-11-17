2e689b15c5716ea2d31b32baa345386c997a8c0f
Leslie Chang <zz2474@columbia.edu>
2022-11-17 18:11:33 -0500

    add validators

---
+++ html.py
@@ -0,0 +1,39 @@
+template = """<!DOCTYPE html >
+<html lang = "en" >
+<head >
+    <title > Office Hour < /title >
+    <meta HTTP-EQUIV = "CACHE-CONTROL" CONTENT = "NO-CACHE" >
+    <meta HTTP-EQUIV = "PRAGMA" CONTENT = "NO-CACHE" >
+    <meta charset = "utf-8" >
+    <meta name = "viewport" content = "width=device-width, initial-scale=1" >
+    <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" >
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" > </script >
+    <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" > </script >
+</head >
+<body >
+   <div class = "container-fluid p-0" >
+       <div class = "row no-gutters" >
+           <div class = "col-xs-12" >
+            </div >
+        </div >
+    </div >
+    <div class = "container-fluid" >
+       <div class = "row" >
+            <div class = "col-sm-4" style = "height:800px">
+                <iframe src = "{0}" width = 100% height=100% frameborder="0" marginheight="0"
+                    marginwidth = "0" > Loading…</iframe>
+            </div >
+            <div class = "col-sm-8" style = "height:800px; padding-top:16px;">
+              <iframe name = "testFrame"
+                 src = "https://docs.google.com/spreadsheets/d/{1}/htmlembed/sheet?gid={2}&amp;single=true&amp;widget=true&amp;headers=false"
+                    width = 100 % height =100%></iframe>
+            </div >
+        </div >
+        <script >
+         window.setInterval("reloadIFrame();", 3000)
+          function reloadIFrame() {{
+               document.getElementsByName("testFrame")[0].src = "https://docs.google.com/spreadsheets/d/{1}/htmlembed/sheet?gid={2}&amp;single=true&amp;widget=true&amp;headers=false"
+               }}
+        </script >
+</body >
+</html >"""
---
+++ oh_queue.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from os import path, makedirs
+from shutil import rmtree
+from re import compile
+from sys import exit
+from html import template
+
+
+def main(form: str, sheet: str, html_path: str = "~/html/oh"):
+    # validate urls:
+    form_m = compile(
+        r"^(https?:\/\/)?(docs.google.com\/forms\/d\/.*)")
+    if not form_m.match(form):
+        exit("form url is not valid")
+    form_url = form_m.sub(r"https://\2", form)
+    sheet_m = compile(r"^(https?:\/\/)?(docs.google.com\/spreadsheets\/d\/.*)")
+    if not sheet_m.match(sheet):
+        exit("sheet url is not valid")
+    info = [form_url,
+            compile(r"d/([a-zA-Z0-9_-]+)/edit").findall(sheet)[0],
+            compile(r"gid=(\d+)").findall(sheet)[0]]
+    print(info)
+    html_path = path.expanduser(html_path)
+    rmtree(html_path, ignore_errors=True)
+    makedirs(html_path, mode=0o755, exist_ok=True)
+    with open(path.join(html_path, "index.html"), "w+") as index:
+        index.write(template.format(info[0], info[1], info[2]))
+
+
+if __name__ == "__main__":
+    while True:
+        form_val = input("Google Form URL: ")
+        if form_val:
+            break
+    while True:
+        sheet_val = input("Google Sheet URL: ")
+        if sheet_val:
+            break
+    html_val = input("HTML path(optional): ")
+    if not html_val:
+        html_val = "~/html/oh"
+    main(form_val, sheet_val, html_val)
